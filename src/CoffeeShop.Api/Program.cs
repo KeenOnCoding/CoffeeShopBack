586fb8b2962e28b7c49278803ce219f731bf6450
Sergeii Riabushenko <serhii.riabushenko@sombrainc.com>
2023-01-09 11:40:20 +0200

    DB connection

--- src/CoffeeShop.Api/Program.cs
+++ src/CoffeeShop.Api/Program.cs
@@ -1,25 +1,45 @@
-var builder = WebApplication.CreateBuilder(args);
+using CoffeeShop.Persistance.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        AddServices(builder);
+
+        var app = builder.Build();
+        ConfigureRequestPipeline(app);
+
+        //SeedDatabase(app);
+
+        app.Run();
+    }
+    private static void AddServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
+            x => x.MigrationsAssembly(typeof(Program).GetTypeInfo().Assembly.GetName().Name))
+        );
+
+        builder.Services.AddControllers();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+    }
+    private static void ConfigureRequestPipeline(WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
